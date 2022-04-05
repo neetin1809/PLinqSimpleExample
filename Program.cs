f2142e7fd2360d3356ae1e8f736f0d5457da0fc2
Nitin Vaswani <nitin.vaswani@rediffmail.com>
2022-04-05 23:21:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Diagnostics;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+IEnumerable<int> rvals = Enumerable.Range(0, 100000000);
+var result = rvals.Where(x => x % 12345678 == 0).Select(x => x);
+
+Stopwatch sq = Stopwatch.StartNew();
+
+foreach (var item in result)
+    Console.WriteLine(item);
+Console.WriteLine("Time taken for processing: " + sq.ElapsedMilliseconds + "ms");
+
+var result1 = rvals.AsParallel().Where(x => x % 12345678 == 0).Select(x => x);
+sq.Restart();
+System.Threading.Thread.Sleep(100);
+foreach (var item in result1)
+    Console.WriteLine(item);
+Console.WriteLine("Time taken for processing(With Parallel Linq: " + sq.ElapsedMilliseconds + "ms");
+
